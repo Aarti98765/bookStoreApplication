213a000c6d9e142f8c4c31c045366f688cdb947c
Aarti98765 <aartiahireengit@gmail.com>
2020-06-24 00:11:02 +0530

    MODIFIED : Code by integrating the api of sign up view

--- src/pages/SignUpForm.js
+++ src/pages/SignUpForm.js
@@ -1,24 +1,78 @@
 import React, { Component } from 'react';
 import image from '../images/new.jpg';
+import BookDataLayer from '../components/BookDataLayer';
 import { Link } from 'react-router-dom';
+
+var data = new BookDataLayer();
 
 class SignUpForm extends Component {
   constructor() {
     super();
 
     this.state = {
-      email: '',
+      emailId: '',
       password: '',
-      name: '',
+      userName: '',
+      phoneNumber: '',
+      role: [],
       hasAgreed: false,
-      roleChecked: false
+      roleChecked: false,
+      admin: false,
+      user: false
     };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange(e) {
+  handleSetName = async(e) => {
+    await this.setState({
+      userName : e.target.value
+    })
+  }
+
+  handleSetPassword = async(e) => {
+    await this.setState({
+      password : e.target.value
+    })
+  }
+
+  handleSetPhoneNumber = async(e) => {
+    await this.setState({
+      phoneNumber : e.target.value
+    })
+  }
+
+  handleSetEmailAddress = async(e) => {
+    await this.setState({
+      emailId : e.target.value
+    })
+  }
+
+  handleSelectRoleAdmin = () => {
+    this.setState({
+      admin: !this.state.admin
+    })
+  }
+
+  handleSelectRoleUser = async() => {
+    this.setState({
+      user: !this.state.user
+    })
+  }
+
+  handleSubmitSignUpForm = () => {
+    if (this.state.admin)
+    this.setState({
+      role: this.state.role.push("admin")
+    })
+
+    if (this.state.user)
+    this.setState({
+      role: this.state.role.push("user")
+    })
+
+    data.signUpData(this.state.userName, this.state.password, this.state.emailId, this.state.phoneNumber, this.state.role)
+  }
+
+  handleChange = (e) => {
     let target = e.target;
     let value = target.type === 'checkbox' ? target.checked : target.value;
     let name = target.name;
@@ -26,23 +80,6 @@ class SignUpForm extends Component {
     this.setState({
       [name]: value
     });
-  }
-
-  handleChangeRoleChecked = (e) => {
-    let target = e.target;
-    let value = target.type === 'checkbox' ? target.checked : target.value;
-    let name = target.name;
-
-    this.setState({
-      [name]: value
-    });
-  }
-
-  handleSubmit(e) {
-    e.preventDefault();
-
-    console.log('The form was submitted with the following data:');
-    console.log(this.state);
   }
 
   render() {
@@ -56,26 +93,27 @@ class SignUpForm extends Component {
             <lable className="FormTitle__Link_SignUp">Sign In</lable> or <lable className="FormTitle__Link_SignIn">Sign Up</lable>
           </div>
           <div className="FormCenter">
-            <form onSubmit={this.handleSubmit} className="FormFields">
+            <div className="FormFields">
               <div className="FormField">
-                <label className="FormField__Label" htmlFor="name">Full Name</label>
-                <input type="text" id="name" className="FormField__Input" placeholder="Enter your full name" name="name" value={this.state.name} onChange={this.handleChange} />
+                <label className="FormField__Label">Name</label>
+                <input type="text" id="name" className="FormField__Input" placeholder="Enter your name" onChange={this.handleSetName} />
               </div>
               <div className="FormField">
-                <label className="FormField__Label" htmlFor="password">Password</label>
-                <input type="password" id="password" className="FormField__Input" placeholder="Enter your password" name="password" value={this.state.password} onChange={this.handleChange} />
+                <label className="FormField__Label">Password</label>
+                <input type="password" id="password" className="FormField__Input" placeholder="Enter your password" onChange={this.handleSetPassword} />
               </div>
               <div className="FormField">
-              <label className="FormField__Label" htmlFor="phoneNumber">Phone Number</label>
-              <input type="phoneNumber" id="phoneNumber" className="FormField__Input" placeholder="Enter your phone number" name="email" value={this.state.email} onChange={this.handleChange} />
+              <label className="FormField__Label">Phone Number</label>
+              <input type="phoneNumber" id="phoneNumber" className="FormField__Input" placeholder="Enter your phone number" onChange={this.handleSetPhoneNumber} />
             </div>
               <div className="FormField">
-                <label className="FormField__Label" htmlFor="email">E-Mail Address</label>
-                <input type="email" id="email" className="FormField__Input" placeholder="Enter your email" name="email" value={this.state.email} onChange={this.handleChange} />
+                <label className="FormField__Label">E-Mail Address</label>
+                <input type="email" id="email" className="FormField__Input" placeholder="Enter your email" onChange={this.handleSetEmailAddress} />
               </div>
-              <div>
+              <div className="FormField">
               <label className="CheckboxLabel">Select Role : </label>
-              <input className="FormField_Checkbox_Admin" type="checkbox" name="roleChecked" value={this.state.roleChecked} onChange={this.handleChangeRoleChecked}/><label style={{color:'brown'}}>Admin</label><input className="FormField_Checkbox_User" type="checkbox" name="roleChecked" value={this.state.roleChecked} onChange={this.handleChangeRoleChecked}/><label style={{color:'brown'}}>User</label>
+              <input className="FormField_Checkbox_Admin" type="checkbox" value="Admin" onChange={this.handleSelectRoleAdmin}/><label style={{color:'brown'}}>Admin</label>
+              <input className="FormField_Checkbox_User" type="checkbox" value="User" onChange={this.handleSelectRoleUser}/><label style={{color:'brown'}}>User</label>
               </div>
               <div className="FormField">
                 <label className="FormField__CheckboxLabel">
@@ -83,9 +121,11 @@ class SignUpForm extends Component {
                 </label>
                 </div>
               <div className="FormField">
-                <button className="FormField__Button mr-20">Sign Up</button> <Link to="/SignInForm" className="FormField__Link">I'm already member</Link>
+                <Link to="/SignInForm">
+                <button className="FormField__Button mr-20" onClick={this.handleSubmitSignUpForm}>Sign Up</button></Link> 
+                <Link to="/SignInForm" className="FormField__Link">I'm already member</Link>
               </div>
-            </form>
+            </div>
           </div>
         </div>
       </div>
